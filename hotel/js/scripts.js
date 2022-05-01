4e62d533cdb0d31ceb32ced8b5d4ee952feab7b6
Aubrey Mariano Account <amariano@uvu.edu>
2022-05-01 15:54:56 -0600

    5/1/22

--- hotel/js/scripts.js
+++ hotel/js/scripts.js
@@ -1,8 +1,8 @@
 // what is the path to the JSON file?
-const apiURL = "../hoteldata.json";
+
 
 //Go fetch it and then wait for a response.
-fetch(apiURL)
+fetch("../hoteldata.json")
   .then((response) => response.json())
   .then((myList) => {
     //Once it comes back, display it to the console.
