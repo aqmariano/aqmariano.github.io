1dfa5c30631a5ac246a8ee4647b8244b11b73d5e
Aubrey Mariano Account <amariano@uvu.edu>
2022-05-02 10:29:21 -0600

    5/2/22

--- local_JSON_start/local_JSON_start/js/scripts.js
+++ local_JSON_start/local_JSON_start/js/scripts.js
@@ -1,6 +1,62 @@
-fetch ("testdata.json")
-.then((mydata) => mydata.json())
-.then ((mydata) => {
-    console.log(mydata);
+const d = new Date();
 
-});
+const todayDayNumber = d.getDay();
+
+const weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+const apiURL="//api.openweathermap.org/data/2.5/forecast?zip=84003,us&appid=c01f43538fe87f20cc1cb03b0f82afda&units=imperial";
+
+fetch (apiURL)
+.then((response) => response.json())
+.then ((weatherInfo) => {
+    console.log(weatherInfo);
+
+document.getElementById('place').innerHTML=weatherInfo.city.name;
+
+let mylist= weatherInfo.list;
+
+let forecastDayNumber = todayDayNumber;
+
+forecastDayNumber += 1;
+
+if (forecastDayNumber===7) {
+    forecastDayNumber=0;}
+
+for (i=0;i<mylist.length;i++) {
+
+    let time=mylist[1].dt_txt;
+
+    if (time.includes('19:00:00')){
+
+        console.log("Found an entry with 19:00:00 in the time. It was report "+i+" from the mylist of 40");
+
+        forecastDayNumber +=1;
+        if(forecastDayNumber===7){forecastDayNumber=0;};
+
+        let theDayName=document.createElement("span");
+        theDayName.textContent=weekday(forecastDayNumber);
+
+        let theTemp=document.createElement("p");
+        theTemp.textContent=weatherInfo.list[i].main.temp+"&deg;";
+
+        let iconcode=weatherInfo.list[i].weather[0].icon;
+        let iconPath="//openweathermap.org/img/w/"+iconcode+".png";
+        let theIcon=document.createElement("img");
+        theIcon.src=iconPath;
+
+        let theDay=document.createElement("div");
+        theDay.appendChild(theDayName);
+        theDay.appendChild(theTemp);
+        theDay.appendChild(theIcon);
+
+        document.getElementById("weatherforcast").appendChild(theDay);
+    }//end if
+}//end for
+});//end of "then" fat arrow function
